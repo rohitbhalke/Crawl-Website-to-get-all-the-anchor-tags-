392a382068ba3de3ac8739c8777dd4225573d968
rohitbhalke <bhalker@pun-bhalker-m.ds.ad.adp.com>
2016-11-13 22:26:59 +0530

    Adding the depth option to scrape the website

--- routes/index.js
+++ routes/index.js
@@ -6,93 +6,129 @@ var async = require('async');
 var fs = require('fs');
 var json2csv = require('json2csv');
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  var originalURL = 'https://medium.com/', result=[];
+    var limit, iteration = 1, visitedUrls = [];
 
-  request(originalURL, function(err, response, html){
-      var $ = cheerio.load(html);
-      var anchorTagsObjArray = $('a'), anchorTags=[];
+    limit = process.argv[2] ? parseInt(process.argv[2].split("=")[1]) : 2;
 
-      [].forEach.call(anchorTagsObjArray, function(url){
-          anchorTags.push(url.attribs.href);
-      });
+    console.log("Starting Scrapping with limit : ", limit);
+    var originalURL = 'https://medium.com/', result = [];
 
-      var filteredTags = anchorTags.filter(function(url){
-          return anchorTags.indexOf(url)===anchorTags.lastIndexOf(url);
-      });
+    request(originalURL, function (err, response, html) {
+        var $ = cheerio.load(html);
+        var anchorTagsObjArray = $('a'), anchorTags = [];
 
-      // Remove this visited URL
-      filteredTags.splice(filteredTags.indexOf(originalURL,1));
+        [].forEach.call(anchorTagsObjArray, function (url) {
+            anchorTags.push(url.attribs.href);
+        });
 
-      result.push(originalURL);
+        var filteredTags = uniqueTags(anchorTags);
 
-      var q = async.queue(function(urlToVisit, callback){
-          request(urlToVisit, function(err, response, html) {
-              if(html){
-                  var $ = cheerio.load(html);
-                  var anchorTagsObjArray = $('a'), anchorTags = [];
-                  [].forEach.call(anchorTagsObjArray, function (url) {
-                      anchorTags.push(url.attribs.href);
-                  });
-                  // Till this time we got all the anchor tags, from current page
+        // Remove this visited URL
+        filteredTags.splice(filteredTags.indexOf(originalURL, 1));
 
-                  var filteredTags = anchorTags.filter(function (url) {
-                      return anchorTags.indexOf(url) === anchorTags.lastIndexOf(url);
-                  });
+        result.push(originalURL);
+        visitedUrls.push(originalURL);
 
-                  // filter these tags, as one page can contain multiple duplicate tags
+        var q = async.queue(function (urlToVisit, callback) {
+            if (visitedUrls.indexOf(urlToVisit) == -1) {
+                visitedUrls.push(urlToVisit);
+                request(urlToVisit, function (err, response, html) {
+                    if (html) {
+                        var $ = cheerio.load(html);
+                        var anchorTagsObjArray = $('a'), anchorTags = [];
+                        [].forEach.call(anchorTagsObjArray, function (url) {
+                            anchorTags.push(url.attribs.href);
+                        });
+                        // Till this time we got all the anchor tags, from current page
 
-                  filteredTags.splice(filteredTags.indexOf(urlToVisit, 1));
-                  result.push(originalURL);
-                  [].push.apply(result, filteredTags);
+                        var filteredTags = uniqueTags(anchorTags);
 
-              }
-              callback();
-          });
-      },5);         // setting parallel limit to 5
+                        console.log(urlToVisit);
+                        // filter these tags, as one page can contain multiple duplicate tags
 
-      // push into the queue
-      for(var i=0;i<filteredTags.length;i++){
-          q.push(filteredTags[i]);
-      }
+                        filteredTags.splice(filteredTags.indexOf(urlToVisit, 1));
+                        result.push(originalURL);
+                        [].push.apply(result, filteredTags);
 
+                    }
+                    callback();
+                });
+            }
+            else {
+                callback();
+            }
+        }, 5);         // setting parallel limit to 5
 
-      q.drain = function() {
-          writeFile(result, res);
-      };
+        // push into the queue
+        function pushIntoQueue(filteredTags) {
+            var mediumSiteTags = removeUnnecessaryTags(filteredTags);
+            for (var i = 0; i < mediumSiteTags.length; i++) {
+                q.push(mediumSiteTags[i]);
+            }
+        };
 
-  });
+        pushIntoQueue(filteredTags);        // Push in queue
+        iteration++;
+
+        q.drain = function () {
+            console.log("**************** Drain **************");
+            if (iteration < limit) {
+                iteration++;
+                pushIntoQueue(result);
+            }
+            else
+                writeFile(result, res);
+        };
+
+    });
 
 });
 
 
-var writeFile = function(result, res) {
-    console.log("END");
-    var filtererResult = result.filter(function(url){
+var writeFile = function (result, res) {
+    var filtererResult = result.filter(function (url) {
         return result.indexOf(url) === result.lastIndexOf(url);
     });
     var json = convertToJson(filtererResult);
-    var csv = json2csv({ data: json, fields: ['url'] });
-    fs.writeFile('file.csv', csv, function(err) {
+    var csv = json2csv({data: json, fields: ['url']});
+    fs.writeFile('file.csv', csv, function (err) {
         if (err) throw err;
         console.log('file saved');
-        res.render('index',{title : "CSV Report Generated"});
+        res.render('index', {title: "CSV Report Generated"});
     });
 };
 
-var convertToJson = function(arr){
+var convertToJson = function (arr) {
     var objArr = [];
-    arr.forEach(function(index){
+    arr.forEach(function (index) {
         objArr.push(
             {
-                'url' : index
+                'url': index
             }
         )
     });
     return objArr;
 };
 
+
+var removeUnnecessaryTags = function (arrayOfTags) {
+    // The purpose is to filter out only those urls which belongs to medium.com
+    var mediumSiteTags = arrayOfTags.filter(function (tagUrl) {
+        return tagUrl.indexOf('https://medium.com/') === 0 || tagUrl === 'https://jobs.medium.com' || tagUrl === 'https://legal.medium.com';
+    });
+    return uniqueTags(mediumSiteTags);
+};
+
+var uniqueTags = function (arrayOfTags) {
+    // The purpose is to get unique tags from array
+    var unique = arrayOfTags.filter(function (tagUrl) {
+        return arrayOfTags.indexOf(tagUrl) === arrayOfTags.lastIndexOf(tagUrl);
+    });
+    return unique;
+};
 
 module.exports = router;
